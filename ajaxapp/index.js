8fc1cd81258ed04b3a17e9aeb37ca5ef75331d19
kmakimaki <rskkmkmk@gmail.com>
2022-07-10 12:56:21 +0900

    Async Functionでリファクタリング

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,36 +1,78 @@
 console.log("index.js: loaded");
 
+async function main() {
+  try {
+    const userId = getUserId();
+    console.log(userId);
+    const userInfo = await fetchUserInfo(userId);
+    console.log(userInfo);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch(error) {
+    console.error(`エラーが発生しました (${error})`);
+  }
+  
+  // fetchUserInfo("js-primer-example")
+  //   // ここではJSONオブジェクトで解決されるPromise
+  //   .then((userInfo) => createView(userInfo))
+  //   // ここではHTML文字列で解決されるPromise
+  //   .then((view) => displayView(view))
+  //   // Promiseチェーンでエラーがあった場合はキャッチされる
+  //   .catch((error) => {
+  //     // Promiseチェーンの中で発生したエラーを受け取る
+  //     console.error(`エラーが発生しました (${error})`);
+  //   });
+}
+
+/**
+ * GitHubからユーザー情報を取得する
+ * @param {*} userId 
+ * @returns 
+ */
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  // fetchの返り値のPromiseをreturnする
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
-      console.log(response.status);
+      // console.log(response.status);
       // エラーレスポンスが返されたことを検知する。
       if (!response.ok) {
         console.error("エラーレスポンス", response);
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
       } else {
-        return response.json().then(userInfo => {
-          console.log(userInfo);
-          // HTMLの組み立て
-          const view = escapeHTML`
-            <h4>${userInfo.name} (@${userInfo.login})</h4>
-            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-            <dl>
-            <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-            </dl>
-          `;
-          // HTMLの挿入
-          const result = document.getElementById("result");
-          result.innerHTML = view;
-        });
+        // JSONオブジェクトで解決されるPromiseを返す
+        return response.json();
       }
-    }).catch(error => {
-      console.error(error);
     });
 }
 
+function getUserId() {
+  return document.getElementById("userId").value;
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+    </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
+}
+
+/**
+ * エスケープ用関数
+ * 
+ * @param {*} str 
+ * @returns 
+ */
 function escapeSpecialChars(str) {
   return str
     .replace(/&/g, "&amp;")
